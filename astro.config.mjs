a4105d9f79724653822778b356fba56a33d05493
initoki <sodalgom@naver.com>
2024-06-07 18:16:55 +0900

    feat: db 제거 빌드 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import webVitals from "@astrojs/web-vitals";
 import vercel from '@astrojs/vercel/serverless';
-
-import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), webVitals(), db()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   adapter: vercel(),
 });
