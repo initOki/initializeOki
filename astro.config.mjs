5ffb2e68f3153092fa6192421b953ec2677dcc47
initoki <sodalgom@naver.com>
2024-06-07 17:11:44 +0900

    feat: update config setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import webVitals from "@astrojs/web-vitals";
+import vercel from '@astrojs/vercel/serverless';
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), webVitals(), db()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), webVitals(), db()],
+  adapter: vercel(),
 });
